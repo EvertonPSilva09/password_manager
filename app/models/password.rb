068c2764154078440b68484c81a84c5170b24b15
Everton Silva <everton.p.silva@outlook.com>
2024-03-25 23:47:37 -0300

    feat: add password table columns

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,3 +1,10 @@
+# table_name "passwords"
+# string "url"
+# string "username"
+# string "password"
+# datetime "created_at", null: false
+# datetime "updated_at", null: false
+#
 class Password < ApplicationRecord
   has_many :user_passwords
   has_many :users, through: :user_passwords
