f346e5854a721a69c3bd98b142c600523131d428
Everton Silva <everton.p.silva@outlook.com>
2024-03-25 23:47:56 -0300

    feat: add user table with necessary columns and indexes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# table_name "users"
+# string "email", default: "", null: false
+# string "encrypted_password", default: "", null: false
+# string "reset_password_token"
+# datetime "reset_password_sent_at"
+# datetime "remember_created_at"
+# datetime "created_at", null: false
+# datetime "updated_at", null: false
+# index ["email"], name: "index_users_on_email", unique: true
+# index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
