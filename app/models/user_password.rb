6240bdcda846733ec463d132fdac82b69f38dff3
Everton Silva <everton.p.silva@outlook.com>
2024-03-25 23:47:46 -0300

    feat: dd user_passwords table and indexes

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,3 +1,11 @@
+# table_name "user_passwords"
+# bigint "user_id", null: false
+# bigint "password_id", null: false
+# datetime "created_at", null: false
+# datetime "updated_at", null: false
+# index ["password_id"], name: "index_user_passwords_on_password_id"
+# index ["user_id"], name: "index_user_passwords_on_user_id"
+#
 class UserPassword < ApplicationRecord
   belongs_to :user
   belongs_to :password
