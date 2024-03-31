4fedb20508543dacc79f8483158a85b525624f1b
Everton Silva <everton.p.silva@outlook.com>
2024-03-31 20:18:42 -0300

    feat: add role attribute to UserPassword model and implement role-based permissions

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,5 +1,6 @@
 # table_name "user_passwords"
 # bigint "user_id", null: false
+# string "role",
 # bigint "password_id", null: false
 # datetime "created_at", null: false
 # datetime "updated_at", null: false
@@ -7,6 +8,39 @@
 # index ["user_id"], name: "index_user_passwords_on_user_id"
 #
 class UserPassword < ApplicationRecord
+  # Viewer can only view the password
+  # Editor can view and edit the password
+  # Owner view, edit, delete and share the password
+  ROLES = %w[viewer editor owner].freeze
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: :true, inclusion: { in: ROLES }
+
+  attribute :role, default: :viewer
+
+  def owner?
+    role == "owner"
+  end
+
+  def viewer?
+    role == "viewer"
+  end
+
+  def editor?
+    role == "editor"
+  end
+
+  def editable?
+    owner? || editor?
+  end
+
+  def shareable?
+    owner?
+  end
+
+  def deletable?
+    owner?
+  end
 end
